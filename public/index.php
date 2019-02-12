3e9d663a22dca85e6d7c4c87ff81158d3ecc6fde
Henri de Jong <henridejong@gmail.com>
2019-02-12 22:52:12 +0100

    Update latest MHOOM from xml

--- public/index.php
+++ public/index.php
@@ -1,10 +1,2 @@
 <?php
-/*
- * Just get the latest recording of Met Het Oog Op Morgen
- */
-$xmlString = file_get_contents('http://feeds.nos.nl/MHOOM');
-
-$xml = new SimpleXMLElement($xmlString);
-$result = $xml->xpath('//item[1]/media:content/@url');
-
-print (string) $result[0]['url'] . PHP_EOL;
+print "Nothing to see here";
