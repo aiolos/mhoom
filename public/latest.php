6640343d402c08f0819a1a6d9e20117c95869f80
Henri de Jong <henridejong@gmail.com>
2019-02-12 22:59:11 +0100

    Show player

--- public/latest.php
+++ public/latest.php
@@ -7,4 +7,7 @@ $xmlString = file_get_contents('http://feeds.nos.nl/MHOOM');
 $xml = new SimpleXMLElement($xmlString);
 $result = $xml->xpath('//item[1]/link');
 
-print (string) $result[0] . PHP_EOL;
+echo '<audio controls>';
+echo '<source src="' . (string) $result[0] . '" type="audio/mpeg">';
+echo 'Your browser does not support the audio element.';
+echo '</audio>';
